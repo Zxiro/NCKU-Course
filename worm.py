25ef14171dd8ed170f0adf083ed9527198f248fd
Zxiro <a0963212510@gmail.com>
2019-09-27 00:45:24 +0800

    Enter Department tab

--- worm.py
+++ worm.py
@@ -7,9 +7,21 @@ resp.encoding='utf-8'
 if resp.status_code==requests.codes.ok: #確定連接成功
     course=BeautifulSoup(resp.text,"html.parser")
 # 以 Beautiful Soup 解析 HTML 程式碼
-
 #print(course.prettify())
 
-tags=course.find_all("option",value="E2") #找一個option 他的value是 E2
-for tag in tags:
-    print(tag.string)
+pro=input('請輸入系所代碼:' )
+print(pro)
+num=input('請輸入課程代號:')
+print(num)
+
+tags=course.find_all("a",href="qry001.php?dept_no="+pro) #找一個option 他的value是 E2
+print(tags)
+ee="qry001.php?dept_no="+pro
+eeurl=url + ee
+resp = requests.get(eeurl) #獲取網頁資料
+resp.encoding='utf-8'
+if resp.status_code==requests.codes.ok: #確定連接成功
+    eecourse=BeautifulSoup(resp.text,"html.parser")
+print(eecourse.prettify())
+#usercourse=eecourse.find_all("tr","td",value=num)
+#print(usercourse)
