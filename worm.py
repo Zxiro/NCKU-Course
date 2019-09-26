006486df5008ecf840140b62f64458833b995d2a
zxiro <36260778+zxiro@users.noreply.github.com>
2019-09-26 16:55:32 +0800

    get option E2

--- worm.py
+++ worm.py
@@ -1,19 +1,15 @@
-import requests
-from bs4 import BeautifulSoup 
-#python提供的發送http請求的模組
+import requests #python提供的發送http請求的模組
+from bs4 import BeautifulSoup #bs4為module名稱 Beautiful...為functionname
 
-#bs4為module名稱 Beautiful...為functionname
-# 原始 HTML 程式碼
-html_doc = """
-<html><head><title>Hello World</title></head>
-<body><h2>Test Header</h2>
-<p>This is a test.</p>
-<a id="link1" href="/my_link1">Link 1</a>
-<a id="link2" href="/my_link2">Link 2</a>
-<p>Hello, <b class="boldtext">Bold Text</b></p>
-</body></html>
-"""
-
+url = "http://course-query.acad.ncku.edu.tw/qry/"
+resp = requests.get(url) #獲取網頁資料
+resp.encoding='utf-8'
+if resp.status_code==requests.codes.ok: #確定連接成功
+    course=BeautifulSoup(resp.text,"html.parser")
 # 以 Beautiful Soup 解析 HTML 程式碼
-soup = BeautifulSoup(html_doc, 'html.parser')
-print(soup.prettify())
+
+#print(course.prettify())
+
+tags=course.find_all("option",value="E2") #找一個option 他的value是 E2
+for tag in tags:
+    print(tag.string)
